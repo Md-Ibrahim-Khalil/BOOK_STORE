f6d1ecb9caf3f6210efacdb968158ee00fb22d3f
ibrahim <ibrahimcse.eng@gmail.com>
2024-06-02 17:35:27 +0600

    GenericAPIView & Mixins create api

---
+++ books/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import GenericAPIView
+from books.models import Book 
+from books.api.serializers import BookSerializer
+from rest_framework.mixins import   ListModelMixin,CreateModelMixin
+
+class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
